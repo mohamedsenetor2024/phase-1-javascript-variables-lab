0d15b088b25f9af836231819047b985eda40a9e8
Mohamed <183903448+mohamedsenetor2024@users.nonereply.github.com>
2025-04-07 16:31:08 +0300

    complete Variables lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+const companyName = "Scuber"
+let mostProfitableNeighborhood = "Chelsea";
+let companyCeo = "Susan Smith"
+
+// function(companyName){companyName
+
+// } console.log(companyName)
+
+
+    // describe('companyName', function () {
+    //   it('is set as Scuber', function () {
+    //     expect(companyName).to.equal('Scuber');
+    //   });
